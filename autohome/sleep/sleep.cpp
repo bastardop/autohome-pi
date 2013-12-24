455ff4c9faf0d3c4e86af2acdb70a86525f910e0
bastardop <pi@autohomepi.(none)>
2013-12-24 21:44:23 +0100

    putting two sleep instances into threads an let them run parallel

--- autohome/sleep/sleep.cpp
+++ autohome/sleep/sleep.cpp
@@ -1,19 +1,38 @@
-// this_thread::sleep_for example
-#include <iostream>       // std::cout
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
-#include <time.h> 
+#include <time.h>
+#include <iostream>
+#include <pthread.h>
 
 using namespace std;
-int main() 
-{
-time_t stamp;
-cout << "start " << endl;
-	while(1){
-time(&stamp);  
-cout << "timestamp " << stamp << endl;
 
-    this_thread::sleep_for (chrono::seconds(10));
-  
+void *sleepa(void *arg) {
+	time_t stamp;
+	cout << "start 1 " << endl;
+	while(1){
+		time(&stamp);
+		cout << "timestamp 1 " << stamp << endl;
+		this_thread::sleep_for (chrono::seconds(10));
 	}
+}
+
+void *sleepb(void *arg) {
+	time_t stamp;
+	cout << "start 2 " << endl;
+        while(1){
+ 		this_thread::sleep_for (chrono::seconds(5));
+		time(&stamp);
+		cout << "timestamp 2 (wait 5) " << stamp << endl;
+        }
+}
+
+int main(void){
+	pthread_t sleepaThread, sleepbThread;
+
+	pthread_create(&sleepaThread, NULL, sleepa, NULL);
+	pthread_create(&sleepbThread, NULL, sleepb, NULL);
+
+	pthread_join(sleepaThread, NULL);
+	pthread_join(sleepbThread, NULL);
+
 }
