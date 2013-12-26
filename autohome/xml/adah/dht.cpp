d51ca3afcb802aa10e0b5040e5504131a7cbf7c1
bastardop <christopher.taeufert@icloud.com>
2013-12-26 22:04:36 +0100

    ..

--- autohome/xml/adah/dht.cpp
+++ autohome/xml/adah/dht.cpp
@@ -43,7 +43,7 @@ int MAXTIMINGS = 100;
 
 int bits[250], data[100];
 int bitidx = 0;
-
+float f, h;
 
 using namespace std;
 
@@ -56,14 +56,14 @@ void readDHT(int type, int pin){
         
         // Set GPIO pin to output
 
-        bcm2835_gpio_fsel(17, BCM2835_GPIO_FSEL_OUTP);
+        bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
         
-        bcm2835_gpio_write(17, HIGH);
+        bcm2835_gpio_write(pin, HIGH);
         usleep(500000);  // 500 ms
-        bcm2835_gpio_write(17, LOW);
+        bcm2835_gpio_write(pin, LOW);
         usleep(20000);
         
-        bcm2835_gpio_fsel(17, BCM2835_GPIO_FSEL_INPT);
+        bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT);
         
         data[0] = data[1] = data[2] = data[3] = data[4] = 0;
         
@@ -75,13 +75,13 @@ void readDHT(int type, int pin){
         // read data!
         for (int i=0; i< MAXTIMINGS; i++) {
             counter = 0;
-            while ( bcm2835_gpio_lev(17) == laststate) {
+            while ( bcm2835_gpio_lev(pin) == laststate) {
                 counter++;
                 //nanosleep(1);         // overclocking might change this?
                 if (counter == 1000)
                     break;
             }
-            laststate = bcm2835_gpio_lev(17);
+            laststate = bcm2835_gpio_lev(pin);
             if (counter == 1000) break;
             bits[bitidx++] = counter;
             
@@ -97,18 +97,21 @@ void readDHT(int type, int pin){
         if ((j >= 39) &&
             (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) ) {
             // yay!
-            /*if (type == DHT11)
+            if (type == 11){
+                f = data[2];
+                h = data[0];
+            }
                 
-            if (type == DHT22) {
-                float f, h;
+            if (type == 22) {
+                
                 h = data[0] * 256 + data[1];
                 h /= 10;
                 
                 f = (data[2] & 0x7F)* 256 + data[3];
                 f /= 10.0;
                 if (data[2] & 0x80)  f *= -1;
-                printf("Temp =  %.1f *C, Hum = %.1f \%\n", f, h);
-            }*/
+                
+            }
         }
     
     }
@@ -120,5 +123,5 @@ int main(void){
     cout << "start" << endl;
     //bcm2835_gpio_fsel(17, BCM2835_GPIO_FSEL_OUTP);
     readDHT(11, 17);
-    cout << "temp: " << data[2] << " humi: " << data[0] << endl;
+    cout << "temp: " << f << " humi: " << h << endl;
 }
