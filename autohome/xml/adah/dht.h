9f98ff21f0889505f1b8112d9f855329e96dac38
bastardop <christopher.taeufert@icloud.com>
2013-12-26 23:41:57 +0100

    Revert a97855b..b8c058e
    This rolls back to commit a97855bb05d47f28db3dcd8d34b1f7c969b98793.

--- autohome/xml/adah/dht.h
+++ autohome/xml/adah/dht.h
@@ -6,33 +6,9 @@
 //
 //
 
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <typeinfo>
-#include <stdlib.h>
-#include <sstream>
-
 #ifndef ____dht__
 #define ____dht__
 
-class dhtread {
-    
-private:
-    std::string content;
-    std::vector<temps> temps;
-    void readDHT(int, int);
-    
-public:
-    std::vector<dhtread> getTemp();
-    dhtread(std::string);
-    ~dhtread();
-    
-};
-
-
-
+#include <iostream>
 
 #endif /* defined(____dht__) */
