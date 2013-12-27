17a6582bf75fe5735ceea4cd72ecf74570c8a0f3
bastardop <christopher.taeufert@icloud.com>
2013-12-27 18:40:24 +0100

    ..

--- autohome/adah/bin.cpp
+++ autohome/adah/bin.cpp
@@ -6,7 +6,7 @@
 //
 //
 
-#include "dht.h"
+//#include "dht.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -17,11 +17,21 @@
 #include <time.h>
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>
-
-#include </home/pi/autohome-pi/autohome/database/sqdb.h>
+#include <vector>
+#include <unistd.h>
+//#include </home/pi/autohome-pi/autohome/database/sqdb.h>
 
 using namespace std;
 
+int MAXTIMINGS = 100;
+
+int bits[250], data[100];
+int bitidx = 0;
+
+float f = 0;
+float h = 0;
+int type = 11;
+int pin = 5;
 vector<float> temp;
 
 time_t new_time;
@@ -34,15 +44,86 @@ int main(void){
     while(1){
         time(&new_time);
     cout << "start" << endl;
-        try {temp = readDHT(11, 5); //caling readDHT() from dht.cpp
+        int counter = 0;
+        int laststate = HIGH;
+        int j=0;
+        bitidx = 0;
+        back.clear();
+        try {
+            pinMode(pin,OUTPUT); //set gpio pin to OUTPUT
+        }
+        catch(...){ cout << "11" << endl;}
+        digitalWrite(pin, HIGH);
+        usleep(500000);  // wait 500 ms
+        digitalWrite(pin, LOW);
+        usleep(20000);   // wait 20 ms
+        
+        pinMode(pin, INPUT); // set gpio to INPUT
+        data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+        
+        // wait for pin to drop?
+        while (digitalRead(pin) == 1) {
+            usleep(1);
+        }
+        try {
+            // read data!
+            for (int i=0; i< MAXTIMINGS; i++) {
+                counter = 0;
+                while ( digitalRead(pin) == laststate) {
+                    counter++;
+                    if (counter == 1000)
+                        break;
+                }
+                laststate = digitalRead(pin);
+                if (counter == 1000)
+                    break;
+                bits[bitidx++] = counter;
+                
+                if ((i>3) && (i%2 == 0)) {
+                    // shove each bit into the storage bytes
+                    data[j/8] <<= 1;
+                    if (counter > 200)
+                        data[j/8] |= 1;
+                    j++;
+                }
+            }
+        } catch(...) { return back;}
+        if ((j >= 39) &&
+            (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) ) {
+            // found good data
+            if (type == 11){
+                try{
+                    f = float(data[2]); //DHT11 is sending readable data
+                    h = float(data[0]);}
+                catch(...){
+                    return back;
+                }
+            }
+            
+            if (type == 22) {
+                
+                h = data[0] * 256 + data[1]; //for DHT22 the recieved data need some additional chaning before output
+                h /= 10;
+                
+                f = (data[2] & 0x7F)* 256 + data[3];
+                f /= 10.0;
+                if (data[2] & 0x80)  f *= -1;
+                
+            }
+            back.push_back(f); //writing data into output
+            back.push_back(h);
+        }
+        temp = back; //returning vector with data
+        
+        /*try {temp = readDHT(11, 5); //caling readDHT() from dht.cpp
         } catch(...){
             cout << "fehler beim auslesen" << endl;
-        }
+        }*/
 try {
     cout << "temp: " << temp[0] << "humi " << temp[1] << endl;
 } catch(...){
 cout << "obviusly wrong data" << endl;}     
-
+/*
    sqdb::Db db("/var/www/liteadmin/weather");
         try {
             sqdb::Statement i = db.Query("insert into data (temp, humi, sender_id, time) values (?, ?, ?, ?)");
@@ -58,5 +139,6 @@ cout << "obviusly wrong data" << endl;}
         }
 
         this_thread::sleep_for (chrono::seconds(20));
+ */
     }
 }
