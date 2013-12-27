4573f7f3262fa60e5cfc7e315676fe4b18bcdc8f
bastardop <christopher.taeufert@icloud.com>
2013-12-27 17:45:57 +0100

    database support for adafruit driver and let aafruit read every 20 seconds

--- autohome/adah/bin.cpp
+++ autohome/adah/bin.cpp
@@ -14,13 +14,38 @@
 #include <wiringPi.h>
 #include <iostream>
 #include <stdint.h>
+#include <time.h>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>
+
+#include </home/pi/autohome-pi/autohome/database/sqdb.h>
 
 using namespace std;
 
 vector<float> temp;
 
+time_t new_time;
+
 int main(void){
+    while(1){
+        time(&new_time);
     cout << "start" << endl;
     temp = readDHT(11, 0); //caling readDHT() from dht.cpp
     cout << "temp: " << temp[0] << "humi " << temp[1] << endl;
+        sqdb::Db db("/var/www/liteadmin/weather");
+        try {
+            sqdb::Statement i = db.Query("insert into data (temp, humi, sender_id, time) values (?, ?, ?, ?)");
+            i.Bind(1, temp[0]);
+            i.Bind(2, temp[1]);
+            i.Bind(3, "999");
+            i.Bind(4, to_string(new_time));
+            i.Next();
+        }
+        catch ( const sqdb::Exception& e ){
+            sqdb::Exception excep(sqdb::Exception&);
+            cout << "Fehler ist aufgetreten " << excep << endl;
+        }
+
+        this_thread::sleep_for (chrono::seconds(20));
+    }
 }
